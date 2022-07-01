ebaea602f310898c8295904f471272d53a8c2934
lyem <rfael403@gmail.com>
2022-06-30 23:33:57 -0300

    update font route

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,11 +3,11 @@ import { createGlobalStyle, css } from 'styled-components'
 const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: 'icomoon';
-    src: url('fonts/icomoon.eot?z2gm6x');
-    src: url('fonts/icomoon.eot?z2gm6x#iefix') format('embedded-opentype'),
-      url('fonts/icomoon.ttf?z2gm6x') format('truetype'),
-      url('fonts/icomoon.woff?z2gm6x') format('woff'),
-      url('fonts/icomoon.svg?z2gm6x#icomoon') format('svg');
+    src: url('/fonts/icomoon.eot?z2gm6x');
+    src: url('/fonts/icomoon.eot?z2gm6x#iefix') format('embedded-opentype'),
+      url('/fonts/icomoon.ttf?z2gm6x') format('truetype'),
+      url('/fonts/icomoon.woff?z2gm6x') format('woff'),
+      url('/fonts/icomoon.svg?z2gm6x#icomoon') format('svg');
     font-weight: normal;
     font-style: normal;
     font-display: block;
