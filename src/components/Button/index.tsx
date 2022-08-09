3b559ca5f597aed77b6c990df50b5a251a70ca34
lyem <rfael403@gmail.com>
2022-08-09 16:36:13 -0300

    update button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,20 @@
 import * as S from './style'
+import { ButtonHTMLAttributes } from 'react'
 
 export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
 const Button = ({
   children,
   size = 'medium',
-  fullWidth = false
+  fullWidth = false,
+  ...props
 }: ButtonProps) => {
   return (
-    <S.Wrapper size={size} fullWidth={fullWidth}>
+    <S.Wrapper size={size} fullWidth={fullWidth} {...props}>
       {!!children && <span>{children}</span>}
     </S.Wrapper>
   )
