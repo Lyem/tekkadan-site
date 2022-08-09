634b923813546a57bbd25e8564de670b681f6793
lyem <rfael403@gmail.com>
2022-08-09 16:47:57 -0300

    update title tag

--- src/components/TitleTag/style.ts
+++ src/components/TitleTag/style.ts
@@ -2,11 +2,12 @@ import styled, { css } from 'styled-components'
 
 export const Tag = styled.h1`
   border-style: solid;
-  border-width: 0px 0px 0px 5px;
+  border-width: 0px 0px 0px 4px;
 
   ${({ theme }) => css`
     padding-left: ${theme.spacings.xxsmall};
     font-size: ${theme.font.sizes.xlarge};
+    line-height: ${theme.font.sizes.xxlarge};
     font-weight: ${theme.font.bold};
     border-color: ${theme.colors.secondary};
     color: ${theme.colors.white};
