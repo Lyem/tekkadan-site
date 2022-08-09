4a00b6e746fc23a4a45f8d93116b098289311b9b
lyem <rfael403@gmail.com>
2022-08-09 16:52:45 -0300

    update overView template

--- src/templates/overView/index.tsx
+++ src/templates/overView/index.tsx
@@ -1,7 +1,28 @@
+import Footer from '../../components/Footer'
+import NavBar from '../../components/NavBar'
 import * as S from './style'
 
-const OverView = () => {
-  return <a>a</a>
+export type OverViewProps = {
+  transparency?: boolean
+  fixed?: boolean
+  children: React.ReactNode
+  background: string
+}
+
+const OverView = ({
+  transparency = false,
+  fixed = false,
+  children,
+  background
+}: OverViewProps) => {
+  return (
+    <S.wrapper>
+      <NavBar transparency={transparency} fixed={fixed} />
+      <S.BackgroundImage background={background}></S.BackgroundImage>
+      <S.ContentWrapper fixed={fixed}>{children}</S.ContentWrapper>
+      <Footer></Footer>
+    </S.wrapper>
+  )
 }
 
 export default OverView
--- src/templates/overView/style.ts
+++ src/templates/overView/style.ts
@@ -1,3 +1,34 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+export type OverViewProps = {
+  fixed?: boolean
+}
+
+export type OverViewPropsBackground = {
+  background: string
+}
 
 export const wrapper = styled.div``
+
+export const BackgroundImage = styled.div<OverViewPropsBackground>`
+  position: absolute;
+  ${({ background }) => css`
+    background-image: url(${background});
+  `}
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 60vh;
+  top: 0;
+  z-index: -1;
+`
+
+export const ContentWrapper = styled.main<OverViewProps>`
+  width: 100%;
+  min-height: 45vh;
+  background: rgba(0, 0, 0, 0.7);
+  backdrop-filter: blur(10px);
+  ${({ fixed }) => css`
+    ${fixed ? `margin-top: 26.6vh;` : `margin-top: 20vh;`}
+  `}
+`
