d2805680678b1a5d943a1b826c89d6d9338f5c23
lyem <rfael403@gmail.com>
2022-08-09 16:49:14 -0300

    update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,13 @@
-import styles from '../styles/404.module.css'
-
-const texto = (
-  <div>
-    Hmm !?
-    <br />
-    Parece que você está perdido
-  </div>
-)
+import NotFound from '../components/404'
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
 
 export default function Custom404() {
   return (
-    <div className={styles.error404_1}>
-      <div className={styles.content}>
-        <div>
-          <h1 className={styles.h1}>404</h1>
-          <h3 className={styles.h3}>{texto}</h3>
-        </div>
-      </div>
-    </div>
+    <>
+      <NavBar />
+      <NotFound />
+      <Footer />
+    </>
   )
 }
