31afa1e4376a080735ea9c735591ef63b115ef88
lyem <rfael403@gmail.com>
2022-10-22 18:19:46 -0300

    update domain

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withLess = require('next-with-less')
 module.exports = withLess({
   reactStrictMode: true,
   images: {
-    domains: ['tekkadan.test']
+    domains: ['https://7288-2804-1964-261-d000-e640-43b6-402-cf7e.sa.ngrok.io']
   },
   compiler: {
     styledComponents: true
