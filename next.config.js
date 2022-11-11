c63a5de3079c93954637d0e130816582de53ae58
lyem <rfael403@gmail.com>
2022-11-11 15:06:11 -0300

    update image conf

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withLess = require('next-with-less')
 module.exports = withLess({
   reactStrictMode: true,
   images: {
-    domains: ['https://7288-2804-1964-261-d000-e640-43b6-402-cf7e.sa.ngrok.io']
+    domains: ['tekkadanapi.mangalivre.ml']
   },
   compiler: {
     styledComponents: true
